231e2f71be242d91e5d39af27d0e73045eb0594a
Sumit Walmiki <walmiki240@gmail.com>
2021-04-21 23:41:40 +0530

    respnsive

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 300) {
+//           $(".black").css("background" , "blue");
+//         }
+
+//         else{
+//             $(".black").css("background" , "#333");
+//         }
+//     })
+//   });
+
+// var myNav = document.getElementsByClassName("nave__div");
+// window.onscroll = function () {
+//   "use strict";
+//   if (document.body.scrollTop >= 200) {
+//       myNav.style.
+//     myNav.classList.add("");
+//     myNav.classList.remove("nav-transparent");
+//   } else {
+//     myNav.classList.add("nav-transparent");
+//     myNav.classList.remove("nav-colored");
+//   }
+// };
